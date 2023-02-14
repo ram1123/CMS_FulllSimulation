b19c98af8c87fdd682e932b87ecb250ae4593366
Ram Krishna Sharma <ram.krishna.sharma@cern.ch>
2023-02-14 09:44:31 +0100

    Added condor job script

--- RunAllSteps.sh
+++ RunAllSteps.sh
@@ -14,7 +14,8 @@ step4=CMSSW_10_2_16_UL
 step5=CMSSW_10_6_17_patch1
 step6=CMSSW_10_6_25
 step7=CMSSW_10_6_26
-outDir=/eos/user/r/rasharma/post_doc_ihep/aTGC/nanoAODnTuples/UL202018_12Jan2023
+# outDir=/eos/user/r/rasharma/post_doc_ihep/aTGC/nanoAODnTuples/temp
+outDir=/eos/user/r/rasharma/post_doc_ihep/aTGC/nanoAODnTuples/aTGC_SignalSamples/WmWpToLmNujj_01j_aTGC_pTW-150toInf_mWV-600to800
 [ ! -d "${outDir}" ] && mkdir -p "${outDir}"
 
 
@@ -34,6 +35,19 @@ do
         scram p CMSSW ${!cmssw}
     fi
     cd ${!cmssw}/src
+    if [ "${!cmssw}" = "CMSSW_10_6_26" ]; then
+        echo "step - ${i}7"
+        eval `scram runtime -sh`
+        git cms-init
+        git cms-merge-topic ram1123:aTGC_VV_reweight_CMSSW100626
+        wget https://raw.githubusercontent.com/ram1123/cmssw/6295127490fb426e7ccbe39b68195677bfdf60f0/initrwgt_aQGC16.header
+        #scram b
+    else
+        echo "step - ${i} "
+    fi
+    echo `pwd`
+    echo ${!cmssw}
+    echo "================================================="
     export HOME=$PWD
     eval `scram runtime -sh`
     scram b
